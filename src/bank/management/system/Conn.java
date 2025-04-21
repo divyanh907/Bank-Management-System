e091c3f019af52564a471cd4ebe8d3c738e332d1
divyansh907 <divyanshjaindpsschool@gmail.com>
2025-04-21 09:50:37 +0530

    bank management system

---
+++ src/bank/management/system/Conn.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.sql.*;
+
+public class Conn{
+    Connection connection;
+    Statement statement;
+    public Conn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","123@456");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
